9a4d96143caf736d79d200da01335e1f41aa55af
alex <me@shooteram.fr>
2020-01-16 11:34:39 +0100

    Update main.go

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ var collation string
 func main() {
 	file := flag.String("file", "", "file location to convert columns's collations from")
 	flag.StringVar(&collation, "collation", "utf8mb4_unicode_ci", "new collation")
+	guid := flag.Bool("without-guid", false, "avoid changing collations of columns with a Doctrine2 \"guid\" comment")
 	flag.Parse()
 
 	if "" == *file {
@@ -25,9 +26,13 @@ func main() {
 		panic(err)
 	}
 
+	regex := `\x60([a-z_]+)\x60 [a-z\(\)0-9]+ COLLATE ([a-z0-9_]+)`
+	if true == *guid {
+		regex = regex + `((.*)\'\(DC2Type:(guid)\)\')?`
+	}
+
 	fmt.Printf(
-		"%s\n", regexp.MustCompile(`\x60([a-z_]+)\x60 [a-z\(\)0-9]+ COLLATE ([a-z0-9_]+)`).
-			ReplaceAllStringFunc(string(data[:]), replace))
+		"%s\n", regexp.MustCompile(regex).ReplaceAllStringFunc(string(data[:]), replace))
 }
 
 func replace(text string) string {
